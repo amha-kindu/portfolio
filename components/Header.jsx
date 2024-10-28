fb4a6083e9d8e18a63f3c20bdf4fa8339073329a
Amha Kindu <amhaznif@gmail.com>
2024-10-28 23:09:25 +0300

    navigation implementation complete

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import Nav from "./Nav";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+            {/* logo */}
+            <Logo/>
+
+            {/* desktop nav & hire me button */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav/>
+                <Link href="/contact">
+                    <Button>Hire me</Button>
+                </Link>
+            </div>
+
+            {/* mobile nav */}
+            <div className="xl:hidden">
+                <MobileNav/>
+            </div>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/Logo.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Logo = () => {
+    return (
+      <Link href="/">
+          <h1 className="text-4xl font-semibold">
+              Amha K<span className="text-accent">.</span>
+          </h1>
+      </Link>
+    )
+  }
+  
+  export default Logo

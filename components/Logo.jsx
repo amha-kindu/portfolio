767b3a58e931e16953117f0b2abf9818667db709
Amha Kindu <amhaznif@gmail.com>
2024-11-06 23:40:19 +0300

    update logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 const Logo = () => {
     return (
-      <Link href="/">
-          <h1 className="text-4xl font-semibold">
+      <Link href="/" className="flex items-end justify-center gap-2">
+          <img src="/logo.svg" alt="logo" width={80} height={80} />
+          <h1 className="text-xl font-semibold">
               Amha K<span className="text-accent">.</span>
           </h1>
       </Link>

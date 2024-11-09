75f7b01b1ac774e798ef620fefa19e37a55c0089
Amha Kindu <amhaznif@gmail.com>
2024-11-09 10:58:04 +0300

    bug fix

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
     return (
       <Link href="/" className="flex items-end justify-center gap-2">
-          <img src="/logo.svg" alt="logo" width={80} height={80} />
+          <Image src="/logo.svg" alt="logo" width={80} height={80} />
           <h1 className="text-xl font-semibold">
               Amha K<span className="text-accent">.</span>
           </h1>

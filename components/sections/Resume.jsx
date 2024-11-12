204bb25bdc1a2e1075c8c3415f84093cd77238bc
Amha Kindu <amhaznif@gmail.com>
2024-11-12 16:41:36 +0300

    minor bug fix

--- components/sections/Resume.jsx
+++ components/sections/Resume.jsx
@@ -210,7 +210,7 @@ const Resume = () => {
                         <TabsContent value="about" className="w-full text-center xl:text-left min-h-[650px]">
                             <div className="flex flex-col gap-[30px]">
                                 <h3 className="text-4xl font-bold">{about.title}</h3>
-                                <p className="max-w-[800px] text-white/60 mx-auto xl:mx-0">
+                                <p className=" text-white/60 mx-auto xl:mx-0">
                                     {about.description}
                                 </p>
                                 <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
